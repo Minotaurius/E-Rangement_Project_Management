ea24b6c0f56607b7e779410f94d032405a83258f
Nate B <nathanbrengman@gmail.com>
2022-08-09 13:45:28 -0500

    cleared out some front js

--- connection/connection.js
+++ connection/connection.js
@@ -3,11 +3,7 @@ const { Sequelize } = require('sequelize');
 const connection = new Sequelize(
     'erangement_data',//database name
     'root', //username
-<<<<<<< HEAD
-    'chps694530', //pw
-=======
     'Empyre', //pw
->>>>>>> 42f9962d4e5a9c17cb80dc941c99708d8563917e
     {
         host: 'localhost',
         dialect: 'mysql',
