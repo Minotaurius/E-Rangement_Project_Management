f2515ed553786b426ced19e94e69f69752330026
Minotaurius <brandonslanger@gmail.com>
2022-08-10 00:05:19 -0500

    Hopefully my other branch isnt broken

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
     res.render ("index")
 })
 
-db.sync({ force: false }).then(() => {
+db.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
     });
 
