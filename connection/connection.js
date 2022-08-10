34b9884db11f7ad88561363a09bedcb784ab8403
Minotaurius <brandonslanger@gmail.com>
2022-08-09 23:49:37 -0500

    Trying to fix code to record demo

--- connection/connection.js
+++ connection/connection.js
@@ -3,11 +3,7 @@ const { Sequelize } = require('sequelize');
 const connection = new Sequelize(
     'erangement_data',//database name
     'root', //username
-<<<<<<< HEAD
     'Superfoxgarfmode22!', //pw
-=======
-    'Empyre', //pw
->>>>>>> 7c61e2190cac97adfabb06e69f539ea516527260
     {
         host: 'localhost',
         dialect: 'mysql',
